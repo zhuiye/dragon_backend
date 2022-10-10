77b85844bbee7217bde99a9c3004d270910b9cde
chenhengcheng <503181017@qq.com>
2022-10-10 15:25:06 +0800

    snake split group remark (unfinished)

--- test.js
+++ test.js
@@ -286,7 +286,7 @@ function routeMatchGrade(grade, routeNumber) {
 
   // if()
 }
-// console.log(generateSnakePosition([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
+console.log(generateSnakePosition([1, 2, 3, 4, 5, 6, 7, 8, 9], 2));
 // console.log(generateSnakePosition([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
 // console.log(generateSnakePosition([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
 
