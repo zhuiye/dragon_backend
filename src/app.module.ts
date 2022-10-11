8c777f2bf68138fd0c65f4ab4553070a041dba85
chenhengcheng <503181017@qq.com>
2022-10-11 22:04:48 +0800

    chore:clean note

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompetitionModule } from './competition/competition.module'
-// import { DatabaseModule } from './database/database.module';
 import { CompetitionItemModule } from './competition-item/competition-item.module';
 import { CompetitionItemSortModule } from './competition-item-sort/competition-item-sort.module';
 
+const ormConfig=require('../ormConfig.json')
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'dragon_sys',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(ormConfig),
     CompetitionModule,
     CompetitionItemModule,
     CompetitionItemSortModule,
