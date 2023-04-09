74a9a124d7b2a9739079171f024564e0a1da4bee
chenhengcheng <503181017@qq.com>
2023-04-09 21:27:09 +0800

    fix bug

--- src/modules/check-list/check-list.module.ts
+++ src/modules/check-list/check-list.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CheckListService } from './check-list.service';
 import { CheckListController } from './check-list.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CheckList } from './entities/check-list.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CheckListModule])],
+  imports: [TypeOrmModule.forFeature([CheckList])],
   controllers: [CheckListController],
   providers: [CheckListService]
 })
